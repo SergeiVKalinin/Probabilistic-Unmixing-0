b4388b94dc70dae0902478928265e10e8ea6738b
Sergei V. Kalinin <54861187+SergeiVKalinin@users.noreply.github.com>
2026-09-06 11:29:55 -0400

    Expose fitting interface

--- src/probunmix/__init__.py
+++ src/probunmix/__init__.py
@@ -5,14 +5,18 @@ concentrations and progressively richer spectral knowledge.
 """
 
 from .data import UnmixingData
+from .results import UnmixingResult
 from .synthetic import generate_example
 from .io import from_arrays
+from .fit import fit
 
 
 __version__ = "0.1.0"
 
 __all__ = [
     "UnmixingData",
+    "UnmixingResult",
     "generate_example",
     "from_arrays",
+    "fit",
 ]
